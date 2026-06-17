a893b6b24c8928376edf5759e87b29fae7b4625c
dryfer <37795536+dominikgar@users.noreply.github.com>
2026-06-17 13:58:45 +0200

    fix: include Clerk frontend API routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -20,6 +20,7 @@ export default clerkMiddleware((auth, request) => {
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)'
+    '/(api|trpc)(.*)',
+    '/__clerk/(.*)'
   ]
 };
